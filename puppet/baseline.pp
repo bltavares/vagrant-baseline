9697d98366c3c1aedb3b3395ac01d4e467d044d6
Bruno Tavares <btavare@thoughtworks.com>
2013-02-21 16:53:36 -0300

    Put rbenv on the path

--- puppet/baseline.pp
+++ puppet/baseline.pp
@@ -15,6 +15,11 @@ node default {
     include baseline::buildessential
     class { 'rbenv': }
     rbenv::plugin { 'sstephenson/ruby-build': }-> rbenv::build { '1.9.3-p385': global => true }
+
+    file {
+      '/home/vagrant/.host_specific':
+        content => "[[ -f /etc/profile.d/rbenv.sh ]] && source /etc/profile.d/rbenv.sh"
+    }
   }
 
   if $hostname =~ /nodejs/ {
