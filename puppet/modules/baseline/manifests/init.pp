a72895ce8aca639903c8c133ba4b329aee3a21b7
Bruno Tavares <btavare@thoughtworks.com>
2013-02-21 15:05:05 -0300

    Revert "Put variables to point to vagrant home"
    This reverts commit 57e6eb66661e417cd51f5d8db198de15479dffd2.

--- puppet/modules/baseline/manifests/init.pp
+++ puppet/modules/baseline/manifests/init.pp
@@ -3,7 +3,6 @@ class baseline {
   include git
   include apt::update
 
-  $vagrant_home = "/home/vagrant"
   $packages = [
     'aptitude',
     'curl',
@@ -27,24 +26,22 @@ class baseline {
 
   exec {
     'set up default settings':
-    command => "bash -c 'HOME=${vagrant_home} curl https://gist.github.com/bltavares/2706792/raw/post-install.sh | bash'",
-    cwd     => $vagrant_home,
-    creates => "${vagrant_home}/.Xmodmap",
+    command => 'curl https://gist.github.com/bltavares/2706792/raw/post-install.sh | bash',
+    cwd     => '/home/vagrant',
     require => [Package['curl'], Package['git-core']],
     user    => 'vagrant',
     ;
 
   'download dot-files':
-    command => "git clone https://github.com/bltavares/dot-files.git ${vagrant_home}/dot-files",
-    creates => "${vagrant_home}/dot-files",
+    command => 'git clone https://github.com/bltavares/dot-files.git /home/vagrant/dot-files',
+    creates => '/home/vagrant/dot-files',
     require => Exec['set up default settings'],
     user    => 'vagrant',
     ;
 
   'install-dot-files':
-    command => 'bash -c "HOME=${vagrant_home} ./install.sh"',
-    cwd     => "${vagrant_home}/dot-files",
-    creates => "${vagrant_home}/.oh-my-zsh",
+    command => 'bash -c "HOME=/home/vagrant ./install.sh"',
+    cwd     => '/home/vagrant/dot-files',
     require => Exec['download dot-files'],
     user    => 'vagrant',
     ;
