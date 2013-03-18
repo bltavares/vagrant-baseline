5c15de80277092f7238764d10de44be61d521757
Bruno Tavares <bruno.exz@gmail.com>
2013-03-18 11:19:56 -0300

    Bump nodejs to version 0.10.0

--- puppet/modules/baseline/manifests/lang/nodejs.pp
+++ puppet/modules/baseline/manifests/lang/nodejs.pp
@@ -1,8 +1,11 @@
 class baseline::lang::nodejs {
 
+  include apt
+
   apt::ppa { 'ppa:chris-lea/node.js': }
-  class { '::nodejs':
-    require => Apt::Ppa['ppa:chris-lea/node.js']
+  package { 'nodejs':
+    ensure  => 'latest',
+    require => Apt::Ppa['ppa:chris-lea/node.js'],
   }
 
 }
--- puppet/modules/vendors/nodejs/tests/npm.pp
+++
@@ -1,6 +0,0 @@
-include 'nodejs'
-
-nodejs::npm { '/tmp/npm:express':
-  ensure  => present,
-  version => '2.5.9',
-}
