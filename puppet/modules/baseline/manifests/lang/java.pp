880760d756399ae51af9475f6d1c7c7766a8e1ab
Bruno Tavares <bruno.exz@gmail.com>
2013-09-23 11:49:00 -0300

    Introduce Ant on java provision. Relates to #29

--- puppet/modules/baseline/manifests/lang/java.pp
+++ puppet/modules/baseline/manifests/lang/java.pp
@@ -2,8 +2,12 @@ class baseline::lang::java {
   include java7
 
   $baseline_user = hiera('baseline_user', 'vagrant')
+
   $maven_basename = 'apache-maven-3.1.0'
   $maven_url = "http://www.us.apache.org/dist/maven/maven-3/3.1.0/binaries/${maven_basename}-bin.tar.gz"
+
+  $ant_basename = 'apache-ant-1.9.2'
+  $ant_url = "http://www.us.apache.org/dist//ant/binaries/${ant_basename}-bin.tar.gz"
 
   exec {
     'Download maven':
@@ -12,9 +16,19 @@ class baseline::lang::java {
       cwd     => '/opt',
       require => Package['curl'],
       ;
+    'Download ant':
+      command => "/usr/bin/curl ${ant_url} | /bin/tar xzv",
+      creates => "/opt/${ant_basename}",
+      cwd     => '/opt',
+      require => Package['curl'],
+      ;
     'source maven bin':
       command => "/bin/echo '[[ -f /etc/profile.d/maven.sh ]] && source /etc/profile.d/maven.sh' >> /home/${baseline_user}/.zshenv",
       unless  => "/bin/grep maven.sh /home/${baseline_user}/.zshenv 2> /dev/null",
+      ;
+    'source ant bin':
+      command => "/bin/echo '[[ -f /etc/profile.d/ant.sh ]] && source /etc/profile.d/ant.sh' >> /home/${baseline_user}/.zshenv",
+      unless  => "/bin/grep ant.sh /home/${baseline_user}/.zshenv 2> /dev/null",
       ;
   }
 
@@ -22,10 +36,18 @@ class baseline::lang::java {
     '/etc/profile.d/maven.sh':
       source => 'puppet:///modules/baseline/maven.sh'
       ;
+    '/etc/profile.d/ant.sh':
+      source => 'puppet:///modules/baseline/ant.sh'
+      ;
     '/opt/maven':
       ensure  => 'link',
       target  => "/opt/${maven_basename}",
       require => Exec['Download maven'],
+      ;
+    '/opt/ant':
+      ensure  => 'link',
+      target  => "/opt/${ant_basename}",
+      require => Exec['Download ant'],
       ;
   }
 }
