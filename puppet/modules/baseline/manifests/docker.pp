11b3751e6c49f6d912d9845e7026e38009a4152b
Bruno Tavares <bruno.exz@gmail.com>
2014-01-27 15:53:45 -0500

    Install docker

--- puppet/modules/baseline/manifests/docker.pp
+++ puppet/modules/baseline/manifests/docker.pp
@@ -6,8 +6,22 @@ class baseline::docker {
     'dkms',
   ]
 
-  package { $docker_kernel:
-    ensure => 'latest',
+  apt::source { 'docker.io':
+    include_src => false,
+    key      => '36A1D7869245C8950F966E92D8576A8BA88D21E9',
+    location => 'http://get.docker.io/ubuntu',
+    release  => 'docker',
+    repos => 'main',
+  }
+
+  package {
+    $docker_kernel:
+      ensure => 'latest',
+      ;
+    'lxc-docker':
+      ensure => 'latest',
+      require => [Apt::Source['docker.io'], Package[$docker_kernel]],
+      ;
   }
 
 }
